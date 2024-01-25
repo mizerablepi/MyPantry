e19842f59fde0a5c7373e0c12eb98c3c8448937e
maaz <mizerablepi@gmail.com>
2024-01-25 17:29:38 +0530

    add token globally

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'token' => csrf_token(),
         ];
     }
 }
