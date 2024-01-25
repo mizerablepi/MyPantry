273388c9d030a8109613b5d5bff5cbfc10e1adca
maaz <mizerablepi@gmail.com>
2024-01-25 17:29:55 +0530

    change home route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/pantry';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
