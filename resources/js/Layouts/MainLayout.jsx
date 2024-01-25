0c69a80196c23f7692f10cc652dcdc6d0e37da6b
maaz <mizerablepi@gmail.com>
2024-01-25 17:32:39 +0530

    add token

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import Aside from "@/Components/Aside";
 
-function MainLayout({ inner }) {
+function MainLayout({ inner, token = "" }) {
   return (
     <>
       <div className="relative bg-yellow-50 overflow-hidden min-h-screen">
-        <Aside />
+        <Aside token={token} />
 
         <main className="md:ml-60 max-h-screen overflow-auto">{inner}</main>
       </div>
