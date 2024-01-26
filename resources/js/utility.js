e993ab3dd163f31cf1c203980342f0b03b40b6c9
maaz <mizerablepi@gmail.com>
2024-01-26 19:52:54 +0530

    add utility function

--- resources/js/utility.js
+++ resources/js/utility.js
@@ -5,3 +5,9 @@ export async function fetchItemName(string, signal) {
   const data = await res.json();
   return data;
 }
+
+export async function fetchRecipes(string, signal) {
+  const res = await fetch(`/api/recipes?ingredients=${string}`, { signal });
+  const data = await res.json();
+  return data;
+}
