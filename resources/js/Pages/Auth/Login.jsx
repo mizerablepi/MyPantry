85e81311fd55e6b7685e0f73dd4c626d04f14f34
maaz <mizerablepi@gmail.com>
2024-01-27 19:39:34 +0530

    add register link

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -88,6 +88,12 @@ export default function Login({ status, canResetPassword }) {
                             Forgot your password?
                         </Link>
                     )} */}
+          <Link
+            href={route("register")}
+            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            New User?
+          </Link>
 
           <PrimaryButton className="ms-4" disabled={processing}>
             Log in
