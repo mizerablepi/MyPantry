cdf848d73fdec538520f5ca1ada336ed2ca8215d
maaz <mizerablepi@gmail.com>
2024-01-27 15:22:44 +0530

    docker 3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,7 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 npm install
+npm run build
 
 echo "Caching config..."
 php artisan config:cache
