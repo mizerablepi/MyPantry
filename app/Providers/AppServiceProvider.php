d1007e57b53e10f66ce1af5bea38daf0ca50a2d4
maaz <mizerablepi@gmail.com>
2024-01-27 17:34:19 +0530

    docker 17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-            $url->forceScheme('https');
+        $url->forceScheme('https');
     }
 }
