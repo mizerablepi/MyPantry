ec076300c0e46cb80d987cdb57068ee56da7ea14
maaz <mizerablepi@gmail.com>
2024-01-27 16:23:47 +0530

    docker 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('APP_ENV') == 'local') {
+        if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
     }
